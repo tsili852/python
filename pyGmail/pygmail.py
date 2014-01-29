b58f771da6c2df36ef0f4b2695c03a91774fcd0f
WorkMe <tsilivisn@unisystems.gr>
2014-01-29 17:16:40 +0200

    Added some methods

--- pyGmail/pygmail.py
+++ pyGmail/pygmail.py
@@ -8,7 +8,8 @@
 # Copyright:   (c) tsili852 2014
 #-------------------------------------------------------------------------------
 
-import imaplib
+import imaplib, re, smtplib
+from email.parser import HeaderParser
 
 class pygmail:
 
@@ -18,6 +19,9 @@ class pygmail:
         self.M = None
         self.response = None
         self.mailboxes = []
+
+        self.SMTP_SERVER = 'smtp.gmail.com'
+        self.SMTP_PORT = 587
 
     def login(self, username, password):
         self.M = imaplib.IMAP4_SSL(self.IMAP_SERVER, self.IMAP_PORT)
@@ -41,6 +45,62 @@ class pygmail:
         rc, self.response = self.M.create(mailbox)
         return rc
 
-    def delete_mailbox(set, mailbox):
+    def delete_mailbox(self, mailbox):
         rc, response = self.M.delete(mailbox)
         return rc
+
+    def get_mail_count(self, folder='Inbox'):
+        rc, count = self.M.select(folder)
+        return count[0]
+
+#   MESSAGES - The number of messages in the mailbox.
+#   RECENT - The number of messages with the Recent flag set.
+#   UIDNEXT - The next unique identifier value of the mailbox.
+#   UIDVALIDITY - The unique identifier validity value of the mailbox.
+#   UNSEEN - The number of messages which do not have the Seen flag set. """
+
+    def get_unread_count(self, folder):
+        rc, message = self.M.status(folder, "(UNSEEN)")
+        unreadCount = re.search("UNSEEN (\d+)", message[0]).group(1)
+        return unreadCount
+
+
+    def get_mail_headers():
+        resp, data = M.FETCH(1, '(RFC822')
+        msg = HeaderParser().parsestr(data[0][1])
+        return msg['From'], msg['To'], msg['Subject']
+
+
+
+    def get_mails_from(self, uid, folder='Inbox'):
+        status, count = self.M.select(folder, readonly=1)
+        status, response = self.M.search(None, 'FROM', uid)
+        email_ids = [e_id for e_id in response[0].split()]
+        return email_ids
+
+    def get_mail_from_id(self, id):
+        status, response = self.M.fetch(id, '(body[header.fields (subject)])')
+        return response
+
+    def send_email(self, sender, to, subject, body,):
+        body = "" + body + ""
+
+        headers = ["From: " + sender,
+           "Subject: " + subject,
+           "To: " + to,
+           "MIME-Version: 1.0",
+           "Content-Type: text/html"]
+
+        headers = "\r\n".join(headers)
+
+        sesion = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+
+        sesion.ehlo()
+        sesion.starttls()
+        sesion.ehlo()
+        self.login(sender, )
+
+
+
+
+
--- pyGmail/tester.py
+++ pyGmail/tester.py
@@ -11,10 +11,21 @@
 from pygmail import pygmail
 
 g = pygmail()
-g.login('basementproject.gr@gmail.com', 'notorious3')
+g.login('nick.tsilivis@gmail.com', 'notorious3')
 
 print g.response
 
 g.get_mailboxes()
 for item in g.mailboxes:
     print item
+
+print(g.get_mail_count())
+
+print(g.get_unread_count("INBOX"))
+
+id_list = g.get_mails_from("tsilivism")
+
+for mail in id_list:
+    print(g.get_mail_from_id(mail))
+
+g.logout()
